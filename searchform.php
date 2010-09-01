6e530ab3cb9e369c776560cc1592ba20a01aa4a7
Nully <nully.nl@gmail.com>
2010-09-01 18:23:31 +0900

    fix comment

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<?php /* UnifyFramework searchform template */ ?>
+<?php /* UnifyFramework searchform template. */ ?>
 <form id="searchform" action="<?php bloginfo("home"); ?>" method="get">
     <p><input type="text" id="s" name="s" value="<?php echo get_search_query(); ?>" /></p>
     <p><input type="submit" id="search" name="search" value="<?php _e("Search"); ?>" /></p>
