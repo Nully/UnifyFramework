e7b29a612cb5b88ddf0a5b1df5dda55db5e6e028
Nully <nully.nl@gmail.com>
2010-09-08 11:23:35 +0900

    build footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,25 @@
 <?php /* UnifyFramework footer template. */ ?>
+<?php get_sidebar("right"); ?>
+<!-- End contents --></div>
+<!-- End container --></div>
+<div id="footer">
+    <div id="footer-in" class="container_16">
+        <?php if(is_active_sidebar("footer-widget")): ?>
+            <?php dynamic_sidebar("footer-widget"); ?>
+        <?php endif; ?>
+        <?php if(is_active_sidebar("footer-widget-2")): ?>
+            <?php dynamic_sidebar("footer-widget-2"); ?>
+        <?php endif; ?>
+        <?php if(is_active_sidebar("footer-widget-3")): ?>
+            <?php dynamic_sidebar("footer-widget-3"); ?>
+        <?php endif; ?>
+        <?php if(is_active_sidebar("footer-widget-4")): ?>
+            <?php dynamic_sidebar("footer-widget-4"); ?>
+        <?php endif; ?>
+        <address>Copyright © 2010 <a href="<?php bloginfo("home"); ?>"><?php bloginfo("name"); ?></a> - All Rights Reserved<br />
+            Powered by WordPress & the UnifyFramework Theme by <a href="http://blog.nully.org/">Nully</a>.</address>
+    <!-- End footer_in --></div>
+<!-- End footer --></div>
+<?php wp_footer(); ?>
+</body>
+</html>
