bd0b312847d71ba6328c68c99b750691bb980d32
Nully <nully.nl@gmail.com>
2010-09-08 12:37:00 +0900

    call method change dynamic_sidebar to get_footer

--- footer.php
+++ footer.php
@@ -4,18 +4,7 @@
 <!-- End container --></div>
 <div id="footer">
     <div id="footer-in" class="container_16">
-        <?php if(is_active_sidebar("footer-widget")): ?>
-            <?php dynamic_sidebar("footer-widget"); ?>
-        <?php endif; ?>
-        <?php if(is_active_sidebar("footer-widget-2")): ?>
-            <?php dynamic_sidebar("footer-widget-2"); ?>
-        <?php endif; ?>
-        <?php if(is_active_sidebar("footer-widget-3")): ?>
-            <?php dynamic_sidebar("footer-widget-3"); ?>
-        <?php endif; ?>
-        <?php if(is_active_sidebar("footer-widget-4")): ?>
-            <?php dynamic_sidebar("footer-widget-4"); ?>
-        <?php endif; ?>
+        <?php get_footer("sidebar"); ?>
         <address>Copyright © 2010 <a href="<?php bloginfo("home"); ?>"><?php bloginfo("name"); ?></a> - All Rights Reserved<br />
             Powered by WordPress & the UnifyFramework Theme by <a href="http://blog.nully.org/">Nully</a>.</address>
     <!-- End footer_in --></div>
