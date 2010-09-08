555550ae4a35fb45b9292aee1aeac0a3265072b3
Nully <nully.nl@gmail.com>
2010-09-08 15:44:57 +0900

    add textdomain load.

--- functions.php
+++ functions.php
@@ -3,6 +3,10 @@
  * UnifyFramework functions support
  *
  */
+
+// Make theme available for translation
+// Translations can be filed in the /languages/ directory
+load_theme_textdomain( 'unify_framework', TEMPLATEPATH . '/langs' );
 require_once dirname(__FILE__). "/library/initialize.php";
 
 
