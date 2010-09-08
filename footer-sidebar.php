483c0317585f877fcf20fdfadaac40c998c5f026
Nully <nully.nl@gmail.com>
2010-09-08 12:36:20 +0900

    add sidebar calls

--- footer-sidebar.php
+++ footer-sidebar.php
@@ -1,1 +1,13 @@
 <?php /* UnifyFramework footer sidebar template. */ ?>
+<?php if(is_active_sidebar("footer-widget")): ?>
+    <?php dynamic_sidebar("footer-widget"); ?>
+<?php endif; ?>
+<?php if(is_active_sidebar("footer-widget-2")): ?>
+    <?php dynamic_sidebar("footer-widget-2"); ?>
+<?php endif; ?>
+<?php if(is_active_sidebar("footer-widget-3")): ?>
+    <?php dynamic_sidebar("footer-widget-3"); ?>
+<?php endif; ?>
+<?php if(is_active_sidebar("footer-widget-4")): ?>
+    <?php dynamic_sidebar("footer-widget-4"); ?>
+<?php endif; ?>
