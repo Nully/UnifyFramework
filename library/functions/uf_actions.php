6ef59089ce81a9fdff05cd567359f9c8b61db6e7
Nully <nully.nl@gmail.com>
2010-10-12 10:38:50 +0900

    add action hook, theme activated.

--- library/functions/uf_actions.php
+++ library/functions/uf_actions.php
@@ -25,4 +25,16 @@ function uf_footer() {
 }
 
 
+/**
+ * activated UnifyFramework theme action
+ *
+ * @access public
+ */
+function uf_activate_theme() {
+    global $pagenow;
+    if(is_admin() && $pagenow == "themes.php" && isset($_GET["activated"])) {
+        do_action("uf_theme_activated");
+    }
+}
+add_action("init", "uf_activate_theme");
 
