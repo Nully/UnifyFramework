17b9e54c24974de29d18942632da156f27771368
Nully <nully.nl@gmail.com>
2010-10-12 11:58:38 +0900

    custom theme supports ( backgroun, header, post, editor-style ) integrated to theme-supports.

--- library/functions/uf_editor-style.php
+++ library/functions/uf_editor-style.php
@@ -2,32 +2,6 @@
 /**
  * UnifyFramework EditorStyle support
  */
-if(function_exists("add_editor_style")) {
-    add_editor_style("editor-style.css");
-}
-else if(function_exists("add_theme_support")) {
-    add_theme_support("editor-style");
-    add_action("mce_css", "uf_overload_mce_css");
-}
 
-
-/**
- * overload TinyMCE editor style
- *
- * @since 1.0-Beta
- * @return String
- */
-function uf_overload_mce_css($mce_css) {
-    if(!empty($mce_css)) {
-        return $mce_css;
-    }
-
-    $url = get_bloginfo("template_url");
-    $url = rtrim($url, "/"). "/editor-style.css";
-
-    $url = apply_filters("uf_overload_mce_css", $url);
-
-    return $url;
-}
 
 
