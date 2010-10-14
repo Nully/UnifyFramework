922f94a2abdbe7f04aee303554b040d74010d3a2
Nully <nully.nl@gmail.com>
2010-10-14 11:34:33 +0900

    remove class style admin page. add admin page uses action hooks.

--- includes/admin.php
+++ includes/admin.php
@@ -3,120 +3,130 @@
  * UnifyFramework Admin page scripts
  *
  */
-class UF_Admin extends UF_Extension {
-    function UF_Admin() {
-        $this->UF_Extension("UF_Admin", __("Theme Admin", "unify_framework"), __("theme supported theme admin.", "unify_framework"));
-    }
-
-
-    /**
-     * Initialize action hooks
-     *
-     * @access public
-     * @return Void
-     */
-    function init() {
-        add_action("admin_head", array(&$this, "admin_head"));
-        add_action("admin_menu", array(&$this, "register_menu"));
-    }
-
-
-    /**
-     * Admin head callback hook
-     *
-     * @access public
-     * @return Void
-     */
-    function admin_head() {
-        /*<script type="text/javascript">
-            jQuery(function($){ $("") });
-        </script>*/
-    }
-
-
-
-    /**
-     * register Admin menu
-     *
-     */
-    function register_menu() {
-        if(function_exists("add_submenu_page")) {
-            add_submenu_page("themes.php", __("UnifyFramework setting pages", "unify_framework"), __("Theme Options", "unify_framework"), 10, "uf-admin", array(&$this, "form"));
-        }
-    }
-
-
-    /**
-     * Display setting form
-     *
-     */
-    function form() {
-        if(!empty($_POST) && isset($_POST["save_option"])) {
-            uf_update_theme_option();
-        }
-        $options = uf_get_theme_option();
-    ?>
-        <div class="wrap" id="uf_admin">
-            <?php screen_icon("options-general"); ?>
-            <h2><?php _e("UnifyFramework setting page", "unify_framework"); ?></h2>
-            <p><?php _e("setting UnifyFramework theme options.", "unify_framework"); ?></p>
-            <form action="" method="post">
-                <?php wp_nonce_field(); ?>
-                <h3><?php _e("Editor style setting"); ?></h3>
-                <dl>
-                    <dt><?php _e("Editor style setting.", "unify_framework"); ?></dt>
-                    <dd><?php uf_form_checkbox(1, array(
-                        "id" => "uf_allow_editor_css", "name" => "allow_editor_css","label" => __("Allow custom editor style", "unify_framework"),
-                        "checked" => $options["allow_editor_css"]
-                    )); ?><br />
-                    <span class="caution"><?php _e("* custom editor style file path: "); ?><?php echo bloginfo("template_directory"); ?>/editor-style.css</span></dd>
-                </dl>
-
-                <h3><?php _e("Comment post setting.", "unify_framework"); ?></h3>
-                <dl>
-                    <dt><?php _e("Comment", "unify_framework"); ?></dt>
-                    <dd><?php uf_form_checkbox(1, array(
-                        "id" => "uf_comment_for_page", "name" => "comment_for_page", "label" => __("Comment allwod page ?", "unify_framework"),
-                        "checked" => $options["comment_for_page"]
-                    )); ?><br />
-                    <?php uf_form_input("text", $options["comment_allowd_pages"], array( "id" => "uf_comment_allowd_pages", "name" => "comment_allowd_pages" )); ?><br />
-                    <label for="uf_comment_allowd_pages" class="caution"><?php _e("* separated 'comma' for page ID", "unify_framework"); ?></label></dd>
-                </dl>
-
-                <h3><?php _e("Custom Image header setting"); ?></h3>
-                <dl>
-                    <dt><?php _e("Custom ImageHeader", "unify_framework"); ?></dt>
-                    <dd><?php uf_form_checkbox(1, array(
-                        "id" => "uf_display_custom_header_in_front", "name" => "show_custom_header_in_front", "label" => __("display custom header in the home page or front page ?", "unify_framework"),
-                        "checked" => $options["show_custom_header_in_front"]
-                    )); ?></dd>
-                </dl>
-
-                <h3><?php _e("Extensions", "unify_framework"); ?></h3>
-                <dl>
-                    <dt><?php _e("CustomPost extension", "unify_framework"); ?></dt>
-                    <dd><?php uf_form_checkbox(1, array(
-                        "id" => "uf_extension_custom_post", "name" => "extension_custom_post", "label" => __("Enable theme support custom post type.", "unify_framework"),
-                        "checked" => $options["extensions"]["extension_custom_post"]
-                    )); ?><br />
-                    <span class="caution"><?php _e("* is installed Custom Post Type UI, this extension always disable."); ?></span><br />
-
-                    <?php uf_form_checkbox(1, array(
-                        "id" => "uf_extension_post_thumbnail", "name" => "extension_post_thumbnail", "label" => __("Enable theme support post thumbnail."),
-                        "checked" => $options["extensions"]["extension_post_thumbnail"]
-                    )); ?></dd>
-                </dl>
-                <p><input type="submit" name="save_option" value="<?php _e("Save options"); ?>" class="button-primary" /></p>
-            </form>
-        <!-- End wrap --></div>
-    <?php
+function uf_admin_save_options() {
+    if($_POST["save_option"]) {
+        uf_update_option("theme_options", array(
+            "allow_editor_css"     => $_POST["allow_editor_css"],
+            "comment_for_page"     => $_POST["comment_for_page"],
+            "comment_allowd_pages" => $_POST["comment_allowd_pages"],
+            "show_custom_header_in_front" => $_POST["show_custom_header_in_front"],
+            "extensions" => array(
+                "custom_post"    => $_POST["custom_post"],
+                "post_thumbnail" => $_POST["post_thumbnail"],
+            ),
+        ));
     }
 }
+add_action("init", "uf_admin_save_options", 0);
 
 
-if(!isset($uf_admin)) {
-    global $uf_admin;
-    $uf_admin = &new UF_Admin();
+
+/**
+ * admin_init action hook
+ *
+ * @access protected
+ * @return Void
+ */
+function uf_admin_init() {
+    global $pagenow, $plugin_page;
+    if($pagenow != "themes.php")
+        return;
+
+    wp_enqueue_style("uf_admin_css", get_bloginfo("template_url"). "/css/admin.css", array(), UF_VERSION, "all");
+}
+add_action("admin_init", "uf_admin_init");
+
+
+
+/**
+ * admin_head action hook
+ *
+ * @access protected
+ * @return Void
+ */
+function uf_admin_head() {
+}
+add_action("admin_head", "uf_admin_head");
+
+
+
+/**
+ * admin_menu action hook
+ *
+ * @access protected
+ * @return Void
+ */
+function uf_admin_submenu() {
+    if(function_exists("add_submenu_page"))
+        add_submenu_page("themes.php", __("UnifyFramework setting pages", "unify_framework"), __("Theme Options", "unify_framework"), 10, "uf-admin", "uf_admin_setting");
+}
+add_action("admin_menu", "uf_admin_submenu");
+
+
+
+/**
+ * admin submenu action hook
+ *   display theme admin panel.
+ * @access protected
+ * @return Void
+ */
+function uf_admin_setting() {
+    $options = uf_get_option("theme_options");
+?>
+<div class="wrap" id="uf_admin">
+    <?php screen_icon("options-general"); ?>
+    <h2><?php _e("UnifyFramework setting page", "unify_framework"); ?></h2>
+    <p><?php _e("setting UnifyFramework theme options.", "unify_framework"); ?></p>
+    <form action="" method="post">
+        <?php wp_nonce_field(); ?>
+        <h3><?php _e("Editor style setting"); ?></h3>
+        <dl>
+            <dt><?php _e("Editor style setting.", "unify_framework"); ?></dt>
+            <dd><?php uf_form_checkbox(1, array(
+                "id" => "uf_allow_editor_css", "name" => "allow_editor_css","label" => __("Allow custom editor style", "unify_framework"),
+                "checked" => $options["allow_editor_css"]
+            )); ?><br />
+            <span class="caution"><?php _e("* custom editor style file path: "); ?><?php echo bloginfo("template_directory"); ?>/editor-style.css</span></dd>
+        </dl>
+
+        <h3><?php _e("Comment post setting.", "unify_framework"); ?></h3>
+        <dl>
+            <dt><?php _e("Comment", "unify_framework"); ?></dt>
+            <dd><?php uf_form_checkbox(1, array(
+                "id" => "uf_comment_for_page", "name" => "comment_for_page", "label" => __("Comment allwod page ?", "unify_framework"),
+                "checked" => $options["comment_for_page"]
+            )); ?><br />
+            <?php uf_form_input("text", $options["comment_allowd_pages"], array( "id" => "uf_comment_allowd_pages", "name" => "comment_allowd_pages" )); ?><br />
+            <label for="uf_comment_allowd_pages" class="caution"><?php _e("* separated 'comma' for page ID", "unify_framework"); ?></label></dd>
+        </dl>
+
+        <h3><?php _e("Custom Image header setting"); ?></h3>
+        <dl>
+            <dt><?php _e("Custom ImageHeader", "unify_framework"); ?></dt>
+            <dd><?php uf_form_checkbox(1, array(
+                "id" => "uf_display_custom_header_in_front", "name" => "show_custom_header_in_front", "label" => __("display custom header in the home page or front page ?", "unify_framework"),
+                "checked" => $options["show_custom_header_in_front"]
+            )); ?></dd>
+        </dl>
+
+        <h3><?php _e("Extensions", "unify_framework"); ?></h3>
+        <dl>
+            <dt><?php _e("CustomPost extension", "unify_framework"); ?></dt>
+            <dd><?php uf_form_checkbox(1, array(
+                "id" => "uf_extension_custom_post", "name" => "custom_post", "label" => __("Enable theme support custom post type.", "unify_framework"),
+                "checked" => $options["extensions"]["custom_post"]
+            )); ?><br />
+            <span class="caution"><?php _e("* is installed Custom Post Type UI, this extension always disable."); ?></span><br />
+
+            <?php uf_form_checkbox(1, array(
+                "id" => "uf_extension_post_thumbnail", "name" => "post_thumbnail", "label" => __("Enable theme support post thumbnail."),
+                "checked" => $options["extensions"]["post_thumbnail"]
+            )); ?></dd>
+        </dl>
+        <p><input type="submit" name="save_option" value="<?php _e("Save options"); ?>" class="button-primary" /></p>
+    </form>
+<!-- End wrap --></div>
+<?php
 }
 
 
