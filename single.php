35dc31f649e908c4f2e49acdf9035669076cfb88
Nully <nully.nl@gmail.com>
2010-10-20 14:28:26 +0900

    change comment_form calls.

--- single.php
+++ single.php
@@ -20,6 +20,6 @@
     <?php if($wp_query->max_num_pages >= 2): ?>
     <div id="uf-pagenavi" class="clearfix"><?php uf_pagenavi(); ?></div>
     <?php endif; ?>
-    <?php comments_template(); ?>
+    <?php uf_comment_form(); ?>
 <!-- End main-contents --></div>
 <?php get_footer(); ?>
