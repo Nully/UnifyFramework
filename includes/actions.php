8283beae420f26cfbd49797b73ae06713297af0d
Nully <nully.nl@gmail.com>
2010-10-21 17:39:46 +0900

    pagenavi methods move to filters.php

--- includes/actions.php
+++ includes/actions.php
@@ -61,25 +61,6 @@ function uf_unsupported_version_notice() {
 
 
 /**
- * UnifyFramework page navigation actions
- *
- */
-/**
- * Action pagenavi formats
- *
- * @access protected
- * @return Void
- */
-function uf_action_pagenavi_formats() {
-    do_action("pagenavi_formats");
-}
-add_action("uf_pagenavi_formats", "uf_action_pagenavi_formats");
-
-
-
-
-
-/**
  * UnifyFramework Comments actions
  *
  */
