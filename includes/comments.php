6fb17efe0927e724a3a60fc0101b1dd86bde65e7
Nully <nully.nl@gmail.com>
2010-10-21 14:19:07 +0900

    fix comment form actions.

--- includes/comments.php
+++ includes/comments.php
@@ -50,6 +50,8 @@ function uf_trackback_field() {
         "trackback_title" => '<h3><label for="">'. __("Trackback / Ping", "unify_framework") .'</label></h3>',
         "trackback_field" => '<div class="trackback"><input type="text" value="'. get_trackback_url() .'" class="trackback-field" readonly="readonly" /></div>'
     );
+
+    do_action("uf_trackback_field");
     echo join(PHP_EOL, apply_filters("uf_trackback_field", $fields));
 }
 
@@ -118,11 +120,12 @@ function uf_comment_form() {
 	$defaults = array(
 		'fields'               => apply_filters( 'comment_form_default_fields', $fields ),
 		'comment_field'        => '<dt class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label></dt>'.
-                                  '<dd><textarea id="comment-content" name="comment" cols="45" rows="8" aria-required="true"></textarea></dd>',
+                                  '<dd><div class="form-allowed-tags">' . sprintf( __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s' ), ' <code>' . allowed_tags() . '</code>' ). '</div>'.
+                                      '<textarea id="comment-content" name="comment" cols="45" rows="8" aria-required="true"></textarea></dd>',
 		'must_log_in'          => '<p class="must-log-in">' .  sprintf( __( 'You must be <a href="%s">logged in</a> to post a comment.' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</p>',
 		'logged_in_as'         => '<div id="comment-meta" class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>', "unify_framework"), admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</div>',
 		'comment_notes_before' => '<p class="comment-notes">' . __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) . '</p>',
-		'comment_notes_after'  => '<p class="form-allowed-tags">' . sprintf( __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s' ), ' <code>' . allowed_tags() . '</code>' ) . '</p>',
+		'comment_notes_after'  => '',
 		'id_form'              => 'commentform',
 		'id_submit'            => 'submit',
 		'title_reply'          => __( 'Leave a Reply' ),
@@ -137,47 +140,39 @@ function uf_comment_form() {
     if(comments_open()):
     ?>
     <?php do_action("uf_comment_form_before"); ?>
-    <?php do_action("comment_form_before"); ?>
     <div id="respond">
         <h3 id="reply-title"><?php comment_form_title($args["title_reply"], $args["title_reply_to"]); ?></h3>
         <?php if(get_option("comment_registration") && !is_user_logged_in()): ?>
             <?php do_action("uf_comment_form_must_log_in_before"); ?>
-
             <?php echo $args["must_log_in"]; ?>
-
             <?php do_action("uf_comment_form_must_log_in_after"); ?>
-            <?php do_action( 'comment_form_must_log_in_after' ); ?>
+
         <?php else: ?>
             <form action="<?php echo get_site_url(null, "/wp-comments-post.php"); ?>" method="post" id="<?php echo esc_attr($args["id_form"]); ?>">
 
-                <?php echo $args["comment_notes_after"]; ?>
-
                 <?php if(is_user_logged_in ()): ?>
+                    <?php do_action("uf_comment_form_logged_in_before"); ?>
                     <?php echo apply_filters( 'comment_form_logged_in', $args['logged_in_as'], $commenter, $user_identity ); ?>
-                    <?php do_action( 'comment_form_logged_in_after', $commenter, $user_identity ); ?>
+                    <?php do_action( 'uf_comment_form_logged_in_after', $commenter, $user_identity ); ?>
                 <?php endif; ?>
 
                 <?php do_action("uf_comment_form_top"); ?>
-                <?php do_action("comment_form_top"); ?>
 
                 <?php if(!is_user_logged_in()): ?>
-                <?php echo $args["comment_notes_before"]; ?>
-
-                    <?php do_action("ufcomment_form_before_fields"); ?>
-                    <?php do_action("comment_form_before_fields"); ?>
+                    <?php echo $args["comment_notes_before"]; ?>
+                    <?php do_action("uf_comment_form_before_fields"); ?>
                         <?php
                             foreach((array)$args["fields"] as $name => $field) {
                                 echo apply_filters("comment_form_field_{$name}", $field). "\n";
                             }
                         ?>
                     <?php do_action("uf_comment_form_after_fields"); ?>
-                    <?php do_action("comment_form_after_fields"); ?>
 
                 <?php endif; ?>
                 <?php echo apply_filters( 'comment_form_field_comment', $args['comment_field'] ); ?>
+                <?php echo $args["comment_notes_after"]; ?>
 
                 <?php do_action("uf_comment_form_bottom"); ?>
-                <?php do_action("comment_form_bottom"); ?>
 
                 <p class="form-submit">
                     <input name="submit" type="submit" id="<?php echo esc_attr( $args['id_submit'] ); ?>" value="<?php echo esc_attr( $args['label_submit'] ); ?>" />
@@ -187,10 +182,8 @@ function uf_comment_form() {
             </form>
         <?php endif;  ?>
     <!-- End respond --></div>
-    <?php do_action("comment_form_after"); ?>
     <?php do_action("uf_comment_form_after"); ?>
     <?php
     endif;
 }
-
 
