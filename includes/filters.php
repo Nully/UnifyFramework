282aef2b7758a20ec1d8cfe34fd1ac4506f9ea59
Nully <nully.nl@gmail.com>
2010-10-22 10:30:45 +0900

    add filter hooks.

--- includes/filters.php
+++ includes/filters.php
@@ -4,11 +4,39 @@
  *
  */
 /**
+ * Filter header image width.
+ *
+ * @access protected
+ * @param  $width      Int
+ * @return Int
+ */
+function uf_filter_header_image_width($width) {
+    return $width;
+}
+add_action("uf_header_image_width", "uf_filter_header_image_width");
+
+
+
+/**
+ * Filter header image height
+ *
+ * @access protected
+ * @param  $height     Int
+ * @return Int
+ */
+function uf_filter_header_image_height($height) {
+    return $height;
+}
+add_action("uf_header_image_height", "uf_filter_header_image_heigh");
+
+
+
+/**
  * Filter pagenavi formats
  *
  * @access protected
  * @param  $formats    Array    page navigation formats.
- * @return Void
+ * @return Array
  */
 function uf_filter_pagenavi_formats($formats) {
     return $formats;
@@ -16,6 +44,26 @@ function uf_filter_pagenavi_formats($formats) {
 add_filter("uf_pagenavi_formats", "uf_filter_pagenavi_formats");
 
 
+
+
+
+/**
+ * UnifyFramework admin filters
+ * 
+ */
+/**
+ * Filter override mce css.
+ *
+ * filter TinyMCE editor style file URL.
+ *
+ * @access protected
+ * @param  $url        String
+ * @return String
+ */
+function uf_filter_overload_mce_css($url) {
+    return $url;
+}
+add_filter("uf_override_mce_css", "uf_filter_overload_mce_css");
 
 
 
