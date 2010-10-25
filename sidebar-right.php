e759035a06f483ae6e1d24209df38b8eab6c9135
Nully <nully.nl@gmail.com>
2010-10-25 10:19:27 +0900

    fix right sidebar. display default sidebar.

--- sidebar-right.php
+++ sidebar-right.php
@@ -4,6 +4,33 @@
  * if right sidebar registerd, shown right sidebar.
  *
  */
-if(uf_has_sidebar("right-sidebar")): ?>
-    <div id="right-sidebar" class="grid_4"><?php uf_get_sidebar("right-sidebar"); ?></div>
+?>
+<div id="right-sidebar" class="grid_4">
+<?php if(!dynamic_sidebar("right-sidebar")): ?>
+    <div class="widget widget_search">
+        <?php get_search_form(); ?>
+    <!-- End Search --></div>
+
+    <div class="widget widget_calendar">
+        <h3 class="widget-title"><?php _e("Calendar", "unify_framework"); ?></h3>
+        <div class="calendar_wrap"><?php get_calendar(); ?></div>
+    <!-- End Calendar --></div>
+
+    <div class="widget widget_archive">
+        <h3 class="widget-title"><?php _e("Archive", "unify_framework"); ?></h3>
+        <ul>
+        <?php get_archives(); ?>
+        </ul>
+    <!-- End Archive --></div>
+
+    <div class="widget widget_categories">
+        <h3 class="widget-title"><?php _e("Categories", "unify_framework"); ?></h3>
+        <ul><?php wp_list_categories("title_li="); ?></ul>
+    <!-- End category --></div>
+
+    <div class="widget widget_tag_cloud">
+        <h3 class="widget-title"><?php _e("TagCloud", "unify_framework"); ?></h3>
+        <div><?php wp_tag_cloud(); ?></div>
+    <!-- End TagCloud --></div>
 <?php endif; ?>
+</div>
