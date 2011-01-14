ae83051722659fdb0bcd3918501e139cb5e4cd26
Nully <nully.nl@gmail.com>
2011-01-14 14:01:57 +0900

    delete default templates

--- includes/custom-functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * UnifyFramework custom functions.
- *
- * your custom remove more link, remove feed link, etc.
- */
-
--- searchform.php
+++
@@ -1,6 +0,0 @@
-<?php /* UnifyFramework searchform template. */ ?>
-<h3 class="widget-title searchtitle"><label for="search-text"><?php _e("Search"); ?></label></h3>
-<form id="searchform" action="<?php bloginfo("home"); ?>" method="get">
-    <p><input type="text" id="search-text" name="s" value="<?php echo get_search_query(); ?>" /></p>
-    <p><input type="submit" id="search-submit" name="search" value="<?php _e("Search"); ?>" /></p>
-</form>
