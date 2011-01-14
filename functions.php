2b1605901d8097f8e68697ca4d2a7d44627426f6
Nully <nully.nl@gmail.com>
2011-01-14 18:29:56 +0900

    add theme supports

--- functions.php
+++ functions.php
@@ -6,3 +6,24 @@
 // require UnifyFramework scripts
 require_once dirname(__FILE__). "/library/uf-load.php";
 
+/**
+ * WordPress native Theme Supports
+ * 
+ */
+/**
+ * CustomBackground
+ * 
+ */
+add_custom_background($header_callback, $admin_header_callback, $admin_image_div_callback);
+/**
+ * Custom ImageHeader
+ * 
+ */
+add_custom_image_header($header_callback, $admin_header_callback, $admin_image_div_callback);
+/**
+ * EditorStyle
+ * 
+ */
+add_editor_style("editor-style.css");
+
+
