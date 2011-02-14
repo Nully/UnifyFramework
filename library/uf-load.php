28ed0ece1ec94665b49b5699e27828fbbcf21d70
Nully <nully.nl@gmail.com>
2011-02-14 18:39:44 +0900

    add loads

--- library/uf-load.php
+++ library/uf-load.php
@@ -2,8 +2,13 @@
 require_once dirname(__FILE__). "/constants.php";
 
 require_once UF_LIB_PATH. "/actions.php";
+require_once UF_LIB_PATH. "/filters.php";
 require_once UF_LIB_PATH. "/templates.php";
+require_once UF_LIB_PATH. "/widgets/WidgetBase.php";
+require_once UF_LIB_PATH. "/widgets/QueryPosts.php";
+require_once UF_LIB_PATH. "/widgets/ImageGallery.php";
 #require_once UF_LIB_PATH. "";
+
 
 if(is_admin()) {
     require_once UF_LIB_PATH. "/admin.php";
