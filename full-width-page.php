48985964010b1f939367c9cd978bf10e7c55bee8
Nully <nully.nl@gmail.com>
2011-02-16 14:24:34 +0900

    fix template code

--- full-width-page.php
+++ full-width-page.php
@@ -7,6 +7,9 @@
 
 <div id="page_<?php the_ID(); ?>" <?php post_class("page"); ?>>
 <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<?php uf_posted_on(); ?>
+<div class="post-content"><?php the_content(); ?></div>
+<?php uf_post_meta(); ?>
 <!-- End page_<?php the_ID(); ?> --></div>
 
 <?php get_footer(); ?>
