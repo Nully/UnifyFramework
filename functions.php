0ac31402bdc09ab9481d0054c5e511a535a52bb0
Nully <nully.nl@gmail.com>
2011-02-16 11:46:11 +0900

    mod functions

--- functions.php
+++ functions.php
@@ -4,24 +4,20 @@
  */
 /**
  * CustomBackground
- * 
  */
 add_custom_background($header_callback, $admin_header_callback, $admin_image_div_callback);
 /**
  * Custom ImageHeader
- * 
  */
 add_custom_image_header($header_callback, $admin_header_callback, $admin_image_div_callback);
 /**
  * EditorStyle
- * 
  */
 add_editor_style("editor-style.css");
 /**
  * Theme support PostThumbnail
  */
 add_theme_support("post-thumbnails", array( "page", "post" ));
-set_post_thumbnail_size(HEADER_IMAGE_WIDTH, HEADER_IMAGE_HEIGHT, false);
 /**
  * Theme support for PostFormats
  */
@@ -30,5 +26,8 @@ add_theme_support("post-formats", array( "aside", "gallery", "link", "image", "q
  * require UnifyFramework scripts
  */
 require_once dirname(__FILE__). "/library/uf-load.php";
-
+/**
+ * Post thumbnail size
+ */
+set_post_thumbnail_size(HEADER_IMAGE_WIDTH, HEADER_IMAGE_HEIGHT, false);
 
