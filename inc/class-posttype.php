4fe9a41785ac226b3f4a3b0e20848e3051869846
Nully <nully.nl@gmail.com>
2011-04-14 15:50:19 +0900

    class-posttype.php fix select box postbox layout

--- inc/class-posttype.php
+++ inc/class-posttype.php
@@ -11,57 +11,57 @@ class UF_Posttype extends UF_Plugin
 	 * @var    Array
 	 */
     protected $_labels = array(
-        array(
+        "name" => array(
             "name" => "name",
             "label" => "投稿タイプ名",
             "description" => "投稿タイプの複数形のラベル"
         ),
-        array(
+        "singular_name" => array(
             "name" => "singular_name",
             "label" => "単一記事名",
             "description" => "単一記事のラベル"
         ),
-        array(
+        "add_new" => array(
             "name" => "add_new",
             "label" => "新しく追加",
             "description" => "新しく追加するためのラベル"
         ),
-        array(
+        "add_new_item" => array(
             "name" => "add_new_item",
             "label" => "新規追加",
             "description" => "新規追加のラベル"
         ),
-        array(
+        "edit_item" => array(
             "name" => "edit_item",
             "label" => "編集",
             "description" => "編集のためのラベル"
         ),
-        array(
+        "new_item" => array(
             "name" => "new_item",
             "label" => "新規追加",
             "description" => "新規追加のためのラベル"
         ),
-        array(
+        "view_item" => array(
             "name" => "view_item",
             "label" => "表示",
             "description" => "表示するためのラベル"
         ),
-        array(
+        "search_items" => array(
             "name" => "search_items",
             "label" => "検索",
             "description" => "検索の為のラベル"
         ),
-        array(
+        "not_found" => array(
             "name" => "not_found",
             "label" => "NotFound",
             "description" => "NotFoundのラベル"
         ),
-        array(
+        "not_found_in_trash" => array(
             "name" => "not_found_in_trash",
             "label" => "NotFoundInTrash",
             "description" => "ゴミ箱のNotFoundのラベル"
         ),
-        array(
+        "parent_item_colon" => array(
             "name" => "parent_item_colon",
             "label" => "親記事",
             "description" => "親を表すためのラベル"
@@ -179,17 +179,17 @@ class UF_Posttype extends UF_Plugin
      * @var    Array
      */
     protected $_menu_positions = array(
-        array( "pos" => 5, "label" => "投稿の下" ),
-        array( "pos" => 10, "label" => "メディアの下" ),
-        array( "pos" => 15, "label" => "リンクの下" ),
-        array( "pos" => 20, "label" => "ページの下" ),
-        array( "pos" => 25, "label" => "コメントの下" ),
-        array( "pos" => 60, "label" => "最初の区切りの下" ),
-        array( "pos" => 65, "label" => "プラグインの下" ),
-        array( "pos" => 70, "label" => "ユーザーの下" ),
-        array( "pos" => 75, "label" => "ツールの下" ),
-        array( "pos" => 80, "label" => "設定の下" ),
-        array( "pos" => 100, "label" => "2番目の区切りの下" ),
+        array( "var" => 5, "label" => "投稿の下" ),
+        array( "var" => 10, "label" => "メディアの下" ),
+        array( "var" => 15, "label" => "リンクの下" ),
+        array( "var" => 20, "label" => "ページの下" ),
+        array( "var" => 25, "label" => "コメントの下" ),
+        array( "var" => 60, "label" => "最初の区切りの下" ),
+        array( "var" => 65, "label" => "プラグインの下" ),
+        array( "var" => 70, "label" => "ユーザーの下" ),
+        array( "var" => 75, "label" => "ツールの下" ),
+        array( "var" => 80, "label" => "設定の下" ),
+        array( "var" => 100, "label" => "2番目の区切りの下" ),
     );
 
 
@@ -206,6 +206,43 @@ class UF_Posttype extends UF_Plugin
 
 
     /**
+     * post type default values
+     *
+     * @access protected
+     * @var    Array
+     */
+    protected $_defaults = array(
+        "post_type" => null,
+        "label"     => null,
+        "labels"    => array(
+            "name"          => null,
+            "singular_name" => "単一記事",
+            "add_new"       => "新規追加",
+            "add_new_item"  => "新規追加",
+            "edit_item"     => "編集",
+            "new_item"      => "新規追加",
+            "view_item"     => "表示",
+            "search_items"  => "検索",
+            "not_found"     => "見つかりませんでした",
+            "not_found_in_trash" => "ゴミ箱で見つかりませんでした",
+            "parent_item_colon"  => "Panret: ",
+        ),
+        "description" => null,
+        "public"    => 1,
+        "publicly_queryable" => 1,
+        "exclude_form_search" => 0,
+        "show_ui"   => 1,
+        "capability_type" => "post",
+        "hierarchical" => 0,
+        "supports"    => array( "title", "editor", "page-attributes" ),
+        "register_meta_box_cb" => false,
+        "taxonomies" => array(),
+        "menu_position" => 25,
+        "can_export"    => 1,
+    );
+
+
+    /**
      * initialize theme plugin
      *
      * @access public
@@ -213,6 +250,79 @@ class UF_Posttype extends UF_Plugin
     public function init()
     {
         #$this->loadPosttypes();
+    }
+
+
+
+    /**
+     * save post type
+     *
+     * @access protected
+     */
+    public function save_post_type()
+    {
+        if(!$_GET["page"] || $_GET["page"] !== "uf-posttype-add") return;
+
+        if(empty($_POST)) return;
+
+        if(!wp_verify_nonce($_POST["_wpnonce"])) return;
+
+        if(!$_POST["post_type"])
+            return wp_die("投稿タイプ名を入力してください。");
+
+        if(strlen($_POST["post_type"]) > 20)
+            return wp_die("投稿タイプは英数字で20字以内にしてください。");
+
+        $posttype = array(
+            "post_type" => $_POST["post_type"],
+            "label"     => $_POST["label"],
+            "labels"    => $_POST["labels"],
+            "description" => $_POST["description"],
+            "public"    => $_POST["public"],
+            "publicly_queryable" => $_POST["publicly_queryable"],
+            "exclude_form_search" => $_POST["exclude_form_search"],
+            "show_ui"   => $_POST["show_ui"],
+            "capability_type" => $_POST["capability_type"],
+            "hierarchical" => $_POST["hierarchical"],
+            "supports"    => $_POST["supports"],
+            "register_meta_box_cb" => $_POST["register_meta_box_cb"],
+            "taxonomies" => $_POST["taxonomies"],
+            "menu_position" => $_POST["menu_position"],
+            "can_export"    => $_POST["can_export"],
+        );
+
+        $posttype = wp_parse_args($posttype, $this->_defaults);
+        $posttype = array_filter($posttype, "esc_attr");
+        $posttypes = get_option("uf_posttypes", array());
+        array_push($posttypes, $posttype);
+
+        if(update_option("uf_posttypes", $posttypes)) {
+            $url = admin_url("themes.php?page=uf-posttype&message=1");
+            wp_redirect($url);
+            exit;
+        }
+    }
+
+
+    /**
+     * shown admin notice
+     *
+     * @access public
+     */
+    public function notices()
+    {
+        $html = "";
+        switch($_GET["message"]) {
+            case 1:
+                $html = '<div class="success fade updated">'.
+                        '<p>投稿タイプを保存しました。</p>'.
+                        '</div>';
+            break;
+            case 2:
+            break;
+        }
+
+        echo $html;
     }
 
 
@@ -232,13 +342,41 @@ class UF_Posttype extends UF_Plugin
      */
     public function list_posttype()
     {
-        $types = array();
+        $posttypes = get_option("uf_posttypes", array());
 ?>
 <div class="wrap">
 <?php screen_icon(); ?><h2>投稿タイプの設定</h2>
 <p>カスタム投稿タイプの設定を行います。</p>
 
-<?php if(!empty($types)): ?>
+<?php if(!empty($posttypes)): ?>
+
+<table border="0" cellpadding="0" cellspacing="0" class="widefat">
+<thead>
+<tr>
+<th>投稿タイプ名</th>
+<th>説明</th>
+<th>サポート</th>
+<th>エクスポートの可否</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+<th>投稿タイプ名</th>
+<th>説明</th>
+<th>サポート</th>
+<th>エクスポートの可否</th>
+</tr>
+</tfoot>
+
+<?php foreach($posttypes as $type): ?>
+<tr>
+<td><?php echo $type["post_type"]; ?></td>
+<td><?php echo $type["description"]; ?></td>
+<td><?php echo implode(", ", $type["supports"]); ?></td>
+<td><?php echo $type["can_export"] == 1 ? 'はい': "いいえ"; ?></td>
+</tr>
+<?php endforeach; ?>
+</table>
 
 <?php else:
 $url = admin_url("themes.php?page=uf-posttype-add");
@@ -257,6 +395,18 @@ $url = admin_url("themes.php?page=uf-posttype-add");
     public function manage_posttype()
     {
         $id = $_GET["posttype_id"];
+        $posttype = array();
+        if(isset($id) && is_numeric($id)) {
+            $posttypes = get_option("uf_posttypes");
+            $posttype = $posttypes[$id];
+            if(!$posttype) {
+                $posttype = array();
+            }
+        }
+
+        $posttype = wp_parse_args($posttype, $this->_defaults);
+        var_dump($posttype);
+        extract($posttype);
 ?>
 <div class="wrap">
 <?php screen_icon(); ?><h2>投稿タイプの<?php
@@ -264,13 +414,14 @@ $url = admin_url("themes.php?page=uf-posttype-add");
 ?></h2>
 
 <form action="" method="post">
+<?php echo wp_nonce_field(); ?>
 <div class="metabox-holder">
-
-
+<div class="postbox-container" style="width: 49%;">
+<div class="meta-box-sortables ui-sortable">
 <?php
     $this->render_text_fields("投稿タイプ名", array(array(
         "name" => "post_type",
-        "var"  => null,
+        "var"  => $post_type,
         "label" => "投稿タイプ",
         "description" => "英数字20文字以下で構成してください"
     )));
@@ -279,7 +430,7 @@ $url = admin_url("themes.php?page=uf-posttype-add");
 <?php
     $this->render_text_fields("ラベル", array(array(
         "name" => "label",
-        "var"  => null,
+        "var"  => $label,
         "label" => "ラベル",
         "description" => "投稿タイプの複数形の名前を入力してください"
     )));
@@ -287,31 +438,40 @@ $url = admin_url("themes.php?page=uf-posttype-add");
 
 
 <?php
-    $this->render_text_fields("ラベルの設定", $this->_labels, "labels");
+    $field_labels = $this->_labels;
+    foreach($labels as $name => $label) {
+        if(isset($field_labels[$name])) {
+            $field_labels[$name]["var"] = $label;
+        }
+    }
+    $this->render_text_fields("ラベルの設定", $field_labels, "labels");
 ?>
 
 
 <?php
     $this->render_text_fields("説明", array(array(
         "name" => "description",
-        "var" => null,
+        "var" => $desctiption,
         "label" => "投稿タイプの説明",
     )));
 ?>
+<!-- End meta box sortables --></div>
+<!-- End postbox container --></div>
 
 
+<div class="postbox-container" style="width: 49%;">
+<div class="meta-box-sortables ui-sortable">
 <?php
     $this->render_radio_fields(
-        "パブリック", $this->_public, "public", null,
+        "パブリック", $this->_public, "public", $public,
         "ユーザーインターフェースの表示/非表示"
     );
 ?>
 
-
 <?php
     $this->render_radio_fields(
         "query_postsの実行権", $this->_query_posts, "publicly_queryable",
-        null, "フロントエンドでpost_typeのクエリの発行可否"
+        $publicly_queryable, "フロントエンドでpost_typeのクエリの発行可否"
     );
 ?>
 
@@ -319,22 +479,23 @@ $url = admin_url("themes.php?page=uf-posttype-add");
 <?php
     $this->render_radio_fields(
         "検索結果の指定", $this->_exclude_form_search, "exclude_form_search",
-        null, "この投稿タイプを検索結果から除外するかを指定します"
+        $exclude_form_search,
+        "この投稿タイプを検索結果から除外するかを指定します"
     );
 ?>
-
 
 <?php
     $this->render_radio_fields(
         "UIの表示", $this->_show_ui, "show_ui",
-        null, "この投稿タイプを管理画面で表示/非表示の設定"
+        $show_ui, "この投稿タイプを管理画面で表示/非表示の設定"
     );
 ?>
 
 <?php
     $this->render_radio_fields(
         "投稿タイプのチェック", $this->_capability_type, "capability_type",
-        null, "投稿タイプの閲覧・編集・削除権限をチェックするのに使用します"
+        $capability_type,
+        "投稿タイプの閲覧・編集・削除権限をチェックするのに使用します"
     );
 ?>
 
@@ -342,57 +503,51 @@ $url = admin_url("themes.php?page=uf-posttype-add");
 <?php
     $this->render_radio_fields(
         "投稿タイプの階層", $this->_hierarchical, "hierarchical",
-        null, "投稿タイプの階層化可否"
+        $hierarchical, "投稿タイプの階層化可否"
     );
 ?>
-
 
 <?php
     $this->render_checkbox_fields(
         "サポート", $this->_supports, "supports",
-        array()
+        $supports
     );
 ?>
 
 <?php
     $this->render_text_fields("メタボックスの設定",array(array(
         "name" => "register_meta_box_cb",
-        "var" => null,
+        "var" => $register_meta_box_cb,
         "label" => "メタボックスコールバック",
         "description" => "メタボックスのコールバックを指定します。<br />".
                          '特に指する必要がない場合は空のままにしてください。'
     )));
 ?>
 
-
 <?php
     $this->render_checkbox_fields(
-        "タクソノミー", $this->_taxonomies, "taxonomies", array()
+        "タクソノミー", $this->_taxonomies, "taxonomies", $taxonomies
     );
 ?>
 
-
-<p><label for="menu_position">メニュー位置</label>
-<select id="menu_position" name="menu_position">
-<?php foreach($this->_menu_positions as $pos): ?>
-<option value="<?php echo $pos["pos"]; ?>"><?php
-    echo $pos["label"];
-?></option>
-<?php endforeach; ?>
-</select>
-</p>
-
+<?php
+    $this->render_select_fields(
+        "メニュー位置", $this->_menu_positions, "menu_position",
+        $menu_position, "メニューの表示位置"
+    );
+?>
 
 <?php
     $this->render_radio_fields(
         "エクスポートの可否", $this->_can_export, "can_export",
-        null, "この投稿タイプもエクスポートにふくませるか"
+        $can_export, "この投稿タイプもエクスポートにふくませるか"
     );
 ?>
-
+<!-- End meta box sortables --></div>
+<!-- End postbox container --></div>
 <!-- End metabox holder --></div>
 
-<p><input type="submit" value="保 存" class="button-primary" /></p>
+<p style="clear: both;"><input type="submit" value="保 存" class="button-primary" /></p>
 
 </form>
 
@@ -404,5 +559,7 @@ $url = admin_url("themes.php?page=uf-posttype-add");
 
 $uf_posttype = new UF_Posttype();
 add_action("init", array($uf_posttype, "init"));
+add_action("admin_init", array($uf_posttype, "save_post_type"));
+add_action("admin_notices", array($uf_posttype, "notices"));
 add_action("admin_menu", array($uf_posttype, "admin_menu"));
 
