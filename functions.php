b743698618f4d1b430baeba3572ea2da27c9276e
Nully <nully.nl@gmail.com>
2011-04-18 14:56:17 +0900

    functions.php delete comments.

--- functions.php
+++ functions.php
@@ -16,17 +16,3 @@ require_once UF_INC_PATH. "/class-plugin.php";
 require_once UF_INC_PATH. "/class-posttype.php";
 require_once UF_INC_PATH. "/class-widget.php";
 require_once UF_INC_PATH. "/class-post_thumbnail.php";
-
-
-// add_editor_style("editor-style.css");
-/**
- * Theme support PostThumbnail
- */
-// add_theme_support("post-thumbnails", array( "page", "post" ));
-/**
- * Post thumbnail size
- */
-// set_post_thumbnail_size(HEADER_IMAGE_WIDTH, HEADER_IMAGE_HEIGHT, false);
-/**
- * require UnifyFramework scripts
- */
