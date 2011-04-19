0d13f3242d748017b2c47fc9723d142ee0acecca
Nully <nully.nl@gmail.com>
2011-04-19 10:51:09 +0900

    add footer php

---
+++ footer.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+	</div><!-- #main -->
+
+	<div id="footer" role="contentinfo">
+		<div id="colophon">
+
+<?php
+	/* A sidebar in the footer? Yep. You can can customize
+	 * your footer with four columns of widgets.
+	 */
+	get_sidebar( 'footer' );
+?>
+
+			<div id="site-info">
+				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</div><!-- #site-info -->
+
+			<div id="site-generator">
+				<a href="<?php
+                    echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) );
+                ?>" title="<?php
+                    esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' );
+                ?>" rel="generator"><?php
+                    printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' );
+                ?></a> |
+                <a href="<?php
+                    echo esc_url("http://blog.nully.org/");
+                ?>" title="<?php
+                    echo esc_attr_e("UnifyFramework");
+                ?>"><?php
+                    echo esc_attr_e("Theme by Nully.");
+                ?></a>
+			</div><!-- #site-generator -->
+
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
