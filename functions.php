8681960dacef5727ed303e00d4b421c5658fa1fe
Nully <nully.nl@gmail.com>
2011-12-06 18:27:06 +0900

    ちょびっとテスト修正

--- functions.php
+++ functions.php
@@ -5,15 +5,37 @@
 /**
  * @UF_BASE_PATH
  */
-define("UF_BASE_PATH", dirname(__FILE__));
+# define("UF_BASE_PATH", dirname(__FILE__));
 /**
  * @UF_INC_PATH
  */
-define("UF_INC_PATH", UF_BASE_PATH. "/inc");
+# define("UF_INC_PATH", UF_BASE_PATH. "/inc");
 
-
+/*
 require_once UF_INC_PATH. "/class-plugin.php";
 require_once UF_INC_PATH. "/class-posttype.php";
 require_once UF_INC_PATH. "/class-widget.php";
 require_once UF_INC_PATH. "/class-thumbnail.php";
 require_once UF_INC_PATH. "/class-format.php";
+*/
+/**
+ * @UNIFY_BASE_PATH
+ */
+define("UNIFY_BASE_PATH", dirname(__FILE__));
+
+/**
+ * UNIFY_INC_PATH
+ */
+define("UNIFY_INC_PATH", UNIFY_BASE_PATH. "/includes");
+
+
+add_action("init", "unifyframework_init");
+function unifyframework_init()
+{
+    require_once UNIFY_INC_PATH. "/class-dashboard.php";
+    require_once UNIFY_INC_PATH. "/class-widgets.php";
+    $unify_dashboard = new UnifyFramework_Admin_Dashboard();
+    $unify_dashboard->register_theme_page(new UnifyFramework_Admin_Widgets());
+}
+
+
